0d525160e5c80ff1ce24cd631ddbd049cb3d3508
ksahebsara06 <97820501+ksahebsara06@users.noreply.github.com>
2022-01-16 12:20:14 -0700

    Update and rename main-4.py to Passowrd While Loop and Number Guessing Game.py

---
+++ Passowrd While Loop and Number Guessing Game.py
@@ -0,0 +1,43 @@
+password = input ("What is your password?")
+
+while password != "jason" :
+  password = input ("What is your password?")
+print ("Welcome in!")
+
+
+
+
+
+
+
+
+
+
+
+number = float (input ("pick a number"))
+
+while number != 18:
+ number = float (input ("pick a number"))
+ if number == 18:
+    print("great you guessed the password ")
+ elif number < 18:
+   print ("it is too low")
+ else:
+   print ("it is too high")
+
+print ("")
+print ("")
+
+a = 0 
+while a < 10:
+  a = a + 1
+  print (a)
+
+
+a = 0 
+while a < 10:
+ print (a)
+ a = a + 1
+
+
+
